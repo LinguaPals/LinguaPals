145ee76b6242640360298ef3f282187d1bfff091
thom-mill <thomas.miller1130@gmail.com>
2025-09-22 23:25:43 -0400

    Start of dashboard
    Started general layout for dashboard. Still need to get login screen and all that logic

--- LinguaApp/src/App.jsx
+++ LinguaApp/src/App.jsx
@@ -4,31 +4,50 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        {/*This is where the top bar should go*/}
+        <TopBar />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div className="content">
+        <h1 className ="logo">Lingua Pals</h1>
+        <br />
+        <div className="dashboard-cards">
+          <Card title='Card 1' /> {/* Card 1 will be past video history */}
+  
+          <Card title='Card 2' /> {/* Card 2 will have the match me button */}
+    
+          <Card title='Card 3' /> {/* Card 3 will have stats / tbd */}
+        </div>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+      <p>
+        
       </p>
     </>
+  )
+}
+
+import CompanyLogo from "/src/images/temporary_logo.jpeg"
+import ProfileImg from "/src/images/profile.png"
+const TopBar = ({ user_auth }) => {
+  return (
+    <div className="top-bar">
+      <img src={CompanyLogo} className="top-bar-img" />
+      <p>Yo lets fucking go Penjamin pals</p> 
+      <img src={ProfileImg} alt='Profile' className="top-bar-img" />
+   
+    </div>
+  )
+}
+
+const Card = ({ title }) => {
+
+  return (
+    <div className="card">
+      <h2>{title}</h2>
+    </div>
   )
 }
 
