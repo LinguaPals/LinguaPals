a99db01c05ad7eca374bdd8f6c187cf8a7cb23f3
thom-mill <thomas.miller1130@gmail.com>
2025-09-23 11:45:12 -0400

    Improved App flow state
    logic for altering between SignIn and Dashboard

--- LinguaApp/src/App.jsx
+++ LinguaApp/src/App.jsx
@@ -4,7 +4,21 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
+  const user = 1; {/*this is where the google sign in auth state will be */}
+  return (
+    <section>
+      {user ? <Dashboard/>:<SignIn/>}
+    </section>
+    
+  )
+}
 
+function SignIn() {
+  {/* Once we get mongo set up put in the sign in with google feature here, as well as user authentication */}
+  {/* Then change the user var so the App() switches states to Dashboard */}
+}
+
+function Dashboard() {
   return (
     <>
       <div>
@@ -13,7 +27,7 @@ function App() {
       </div>
     
       <div className="content">
-        <h1 className ="logo">Lingua Pals</h1>
+        <h1 className ="logo">Lingua Pals</h1> {/*this will prolly get switched out for custom image */}
         <div className="dashboard-cards">
           <Card title='Card 1' /> {/* Card 1 will be past video history */}
   
@@ -27,6 +41,7 @@ function App() {
   )
 }
 
+{/*component for the top bar*/}
 import CompanyLogo from "/src/images/temporary_logo.jpeg"
 import ProfileImg from "/src/images/profile.png"
 const TopBar = ({ user_auth }) => {
@@ -40,8 +55,8 @@ const TopBar = ({ user_auth }) => {
   )
 }
 
+{/*Card component this will likely have to change a lot but this is a place holder */}
 const Card = ({ title }) => {
-
   return (
     <div className="card">
       <h2>{title}</h2>
