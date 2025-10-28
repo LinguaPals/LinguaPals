9d222c57e9d5fae2ee605a855a6b976f56d2bed4
dennydu2 <166749847+dennydu2@users.noreply.github.com>
2025-10-27 22:15:51 -0400

    fixes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import './App.css'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Dashboard from './pages/dashboard.jsx'
 import SignInPage from './pages/sign_in_page.jsx';
 import LogInPage from './pages/login_page.jsx';
@@ -12,11 +12,13 @@ function App() {
     return (
        <Router>
             <Routes>
+                <Route path="/" element={<LandingPage />} />
                 <Route path="/landing" element={<LandingPage />} />
                 <Route path="/login" element={<LogInPage />} />
                 <Route path="/signup" element={<SignInPage />} />
                 <Route path="/dashboard" element={<Dashboard />} />
                 <Route path="/survey" element={<NewUserSurvey />} />
+                <Route path="*" element={<Navigate to="/" replace />} />
             </Routes>
        </Router>
     )
