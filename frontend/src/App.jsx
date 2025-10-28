145ed2822eec75968a1641b87282acd08daf6253
thom-mill <thomas.miller1130@gmail.com>
2025-10-28 12:59:03 -0400

    fixes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import './App.css'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Dashboard from './pages/dashboard.jsx'
-import SignInPage from './pages/sign_in_page.jsx';
+import SignInPage from './pages/sign_up_page.jsx';
 import LogInPage from './pages/login_page.jsx';
 import NewUserSurvey from './pages/new_user_survey.jsx'
 import LandingPage from './pages/landing_page.jsx'
